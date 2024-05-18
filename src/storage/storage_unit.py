662b9b1624a9f517b7cf00d8817f3defd343efc0
Danpa_cho <danpa725@cau.ac.kr>
2024-05-19 00:17:57 +0900

    fix: remove invalid insert validation logic

--- src/storage/storage_unit.py
+++ src/storage/storage_unit.py
@@ -1,12 +1,16 @@
 import os
+from typing import Union, TypeVar, Generic
+from src.storage.file_manager import JSONFileManager
 
-from typing import Union
-from file_manager import JSONFileManager
+StorageValue = TypeVar("StorageValue", bound=Union[str, int, float, dict, list, None])
 
 
-class Storage:
+class Storage(Generic[StorageValue]):
     """
     Storage class is a base class for all storage units.
+
+    Generic:
+        StorageValue: The type of the storage value.
 
     Attributes:
         root_path: str
@@ -14,14 +18,12 @@ class Storage:
     """
 
     def __init__(self, root_filename: str, label: str = "Storage"):
-
         root = os.path.join(os.getcwd(), root_filename + ".json")
-
         self.root_path = root
         self.label = label
-        self.io = JSONFileManager(root_path=self.root_path, label=label)
-        self.io.load_json()
-        self.db: dict = self.io.read()
+        self._io = JSONFileManager(root_path=self.root_path, label=label)
+        self._io.load_json()
+        self._db: dict[str, StorageValue] = self._io.read()
 
     def check_field_exist(self, field: str) -> bool:
         """
@@ -37,43 +39,13 @@ class Storage:
         """
         return field in self.keys
 
-    def get_field(self, field: str) -> any:
-        """
-        Get the field from the storage unit.
-
-        Args:
-            field: str
-                The field to get.
-
-        Returns:
-            any
-                The field value.
-        """
-        return self.db[field]
-
     def save(self) -> None:
         """
         Save the storage unit to the file.
         """
-        self.io.save_json(self.db)
+        self._io.save_json(self._db)
 
-    def insert(self, field: str, value: Union[str, int, float, dict, list]) -> None:
-        """
-        Insert a field to the storage unit.
-
-        Args:
-            field: str
-                The field to insert.
-            value: Union[str, int, float, dict, list]
-                The value to insert.
-        """
-        if self.check_field_exist(field):
-            print(f"[{self.label}]: Field {field} already exists.")
-            return
-
-        self.db[field] = value
-
-    def delete(self, field: str) -> None:
+    def delete_field(self, field: str) -> None:
         """
         Delete a field from the storage unit.
 
@@ -85,30 +57,35 @@ class Storage:
             print(f"[{self.label}]: Field {field} does not exist.")
             return
 
-        del self.db[field]
+        del self._db[field]
 
     def reset(self) -> None:
         """
         Reset the storage unit.
         """
-        self.db = {}
-        self.io.save_json(self.db)
+        self._db = {}
+        self._io.save_json(self._db)
 
-    def update(self, field: str, value: Union[str, int, float, dict, list]) -> None:
+    def insert_field(self, field: str, value: StorageValue) -> None:
         """
-        Update a field in the storage unit.
+        Update(replace) a field in the storage unit.
 
         Args:
             field: str
                 The field to update.
-            value: Union[str, int, float, dict, list]
+            value: T
                 The value to update.
+        """
+        self._db[field] = value
+
+    def inquire(self, field: str) -> StorageValue:
+        """
+        Inquire the storage unit.
         """
         if not self.check_field_exist(field):
             print(f"[{self.label}]: Field {field} does not exist.")
-            return
-
-        self.db[field] = value
+            return None
+        return self._db[field]
 
     @property
     def keys(self) -> list[str]:
@@ -119,16 +96,29 @@ class Storage:
             list[str]
                 The list of keys.
         """
-        return list(self.db.keys())
+        return list(self._db.keys())
 
-    def inquire(self, field: str) -> Union[str, int, float, dict, list, None]:
+    @property
+    def values(self) -> list[StorageValue]:
         """
-        Inquire the storage unit.
+        Get the values of the storage unit.
+
+        Returns:
+            list[T]
+                The list of values.
         """
-        if not self.check_field_exist(field):
-            print(f"[{self.label}]: Field {field} does not exist.")
-            return None
-        return self.db[field]
+        return list(self._db.values())
+
+    @property
+    def size(self) -> int:
+        """
+        Get the size of the storage unit.
+
+        Returns:
+            int
+                The size of the storage unit.
+        """
+        return len(self._db)
 
     def __repr__(self) -> str:
         return f"[Storage Unit: {self.label}] (root_path={self.root_path})"
