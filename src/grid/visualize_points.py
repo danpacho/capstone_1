3ea9c5bc0affdad6d0f5b4ba137c8956e412877e
Danpa_cho <danpa725@cau.ac.kr>
2024-05-18 12:58:14 +0900

    refactor: visualize duplicated points logic at visualize_points

--- src/grid/visualize_points.py
+++ src/grid/visualize_points.py
@@ -12,11 +12,19 @@ def visualize_points(cell_list: list[GridCell]) -> None:
     plt.figure(figsize=(10, 10))
     plt.axis("equal")
 
+    cell_table: dict[str, int] = {}
+
     for cell in cell_list:
         x_lower = cell.x
         y_lower = cell.y
         x_upper = x_lower + cell.k
         y_upper = y_lower + cell.k
+
+        if cell.id not in cell_table:
+            cell_table[cell.id] = 1
+        else:
+            cell_table[cell.id] += 1
+
         plt.fill_between(
             [
                 x_lower,
@@ -24,8 +32,8 @@ def visualize_points(cell_list: list[GridCell]) -> None:
             ],
             y_lower,
             y_upper,
-            color="gray",
-            alpha=0.5,
+            color="gray" if cell_table[cell.id] == 1 else "red",
+            alpha=0.5 if cell_table[cell.id] == 1 else 1,
         )
 
     plt.show()
