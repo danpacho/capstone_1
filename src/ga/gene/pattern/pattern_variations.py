34a06551fb8a32674bfe358f3318202d15328d64
Danpa_cho <danpa725@cau.ac.kr>
2024-05-22 00:51:58 +0900

    fix: static variable reference issue -> transform to pattern parameter zen function

--- src/ga/gene/pattern/pattern_variations.py
+++ src/ga/gene/pattern/pattern_variations.py
@@ -2,35 +2,43 @@ from math import pi
 
 from src.ga.gene.pattern.pattern_gene import PatternGeneParameter
 
-grid_params = PatternGeneParameter(
-    pattern_type="grid_strict",
-    label="GridStrictTransformation",
-    parameter_boundary_list=[
-        (0.5, 10.0),  # dx
-        (0.5, 10.0),  # dy
-    ],
-    parameter_id_list=["dx", "dy"],
-)
+K = pow(2, 20)
 
-circular_params = PatternGeneParameter(
-    pattern_type="circular_strict",
-    label="CircularStrictTransformation",
-    parameter_boundary_list=[
-        (7.5, 15),  # di
-        (0.5, 10.0),  # dx
-        (pi / 12, pi / 2),  # phi,
-    ],
-    parameter_id_list=["di", "dx", "phi"],
-)
 
-corn_params = PatternGeneParameter(
-    pattern_type="corn",
-    label="CornerTransformation",
-    parameter_boundary_list=[
-        (7.5, 15),  # di
-        (0.5, 10.0),  # dx
-        (pi / 12, pi / 2),  # phi,
-        (1, 7),  # rot_count
-    ],
-    parameter_id_list=["di", "dx", "phi", "rot_count"],
-)
+def grid_params() -> PatternGeneParameter:
+    return PatternGeneParameter(
+        pattern_type="grid_strict",
+        label="GridStrictTransformation",
+        parameter_boundary_list=[
+            (0.5 * K, 10.0 * K),  # dx
+            (0.5 * K, 10.0 * K),  # dy
+        ],
+        parameter_id_list=["dx", "dy"],
+    )
+
+
+def circular_params() -> PatternGeneParameter:
+    return PatternGeneParameter(
+        pattern_type="circular_strict",
+        label="CircularStrictTransformation",
+        parameter_boundary_list=[
+            (7.5 * K, 15 * K),  # di
+            (0.5 * K, 10.0 * K),  # dx
+            (pi / 10, pi / 4),  # phi,
+        ],
+        parameter_id_list=["di", "dx", "phi"],
+    )
+
+
+def corn_params() -> PatternGeneParameter:
+    return PatternGeneParameter(
+        pattern_type="corn",
+        label="CornerTransformation",
+        parameter_boundary_list=[
+            (7.5 * K, 15 * K),  # di
+            (0.5 * K, 10.0 * K),  # dx
+            (pi / 12, pi / 4),  # phi,
+            (2, 7),  # rot_count
+        ],
+        parameter_id_list=["di", "dx", "phi", "rot_count"],
+    )
