4c343d0997f7fbfdec8ab92d4e7ef95cd7e3aa0d
Danpa_cho <danpa725@cau.ac.kr>
2024-05-22 00:51:32 +0900

    fix: static variable reference issue -> transform to shape parameter zen function

--- src/ga/gene/shape/shape_variations.py
+++ src/ga/gene/shape/shape_variations.py
@@ -1,73 +1,88 @@
 from src.ga.gene.shape.shape_gene import ShapeGeneParameter
 
-bbox = (10, 10, 0.25)
+# GRID width size
+K = pow(2, 20)
+# Bounding box, (width, height, resolution)
+bbox = (10 * K, 10 * K, K / 2)
 
-donut_params = ShapeGeneParameter(
-    label="DonutShape",
-    bbox=bbox,
-    a_f=[
-        lambda p, params: (p[0] ** 2 + p[1] ** 2) >= params[0]
-        and (p[0] ** 2 + p[1] ** 2) <= params[1],
-        # x^2 + y^2 >= r_inner^2 and x^2 + y^2 <= r_outer^2
-    ],
-    parameter_id_list=["r_inner", "r_outer"],
-    parameter_boundary_list=[(2, 6), (7, 10)],
-    # 2 <= r_inner <= 6, 7 <= r_outer <= 10
-)
 
-trapezoid_params = ShapeGeneParameter(
-    label="Trapezoid",
-    bbox=bbox,
-    a_f=[
-        lambda p, params: p[1]
-        <= ((params[1] - params[0]) / 10 * (p[0] - 5) + params[1])
-        and p[1] >= -((params[1] - params[0]) / 10 * (p[0] - 5) + params[1])
-        # y <= (k2 - k1) / 10 * (x - 5) + k2 and
-    ],
-    parameter_id_list=["k1", "k2"],
-    parameter_boundary_list=[(2, 5), (1, 5)],
-)
+def donut_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="DonutShape",
+        bbox=bbox,
+        a_f=[
+            lambda p, params: (p[0] ** 2 + p[1] ** 2) >= params[0] ** 2
+            and (p[0] ** 2 + p[1] ** 2) <= params[1] ** 2,
+            # x^2 + y^2 >= r_inner^2 and x^2 + y^2 <= r_outer^2
+        ],
+        parameter_id_list=["r_inner", "r_outer"],
+        parameter_boundary_list=[(2 * K, 6 * K), (7 * K, 10 * K)],
+        # 2 <= r_inner <= 6, 7 <= r_outer <= 10
+    )
 
-circle_params = ShapeGeneParameter(
-    label="CircleShape",
-    bbox=(12.5, 12.5, 0.25),
-    a_f=[
-        lambda p, params: (p[0] ** 2 + p[1] ** 2) <= params[0],
-        # x^2 + y^2 <= r^2
-    ],
-    parameter_id_list=["r"],
-    parameter_boundary_list=[(2, 5)],
-    # 2 <= r <= 5
-)
 
-triangle_params = ShapeGeneParameter(
-    label="TriangleShape",
-    bbox=bbox,
-    a_f=[
-        lambda p, params: p[0] >= 0 and p[1] >= 0 and p[0] + p[1] <= params[0],
-        # x >= 0 and y >= 0 and x + y <= l
-    ],
-    parameter_id_list=["l"],
-    parameter_boundary_list=[(3, 10)],
-    # 2 <= l <= 10
-)
+def trapezoid_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="Trapezoid",
+        bbox=(10 * K, 5 * K, K / 2),
+        a_f=[
+            lambda p, params: p[1]
+            <= ((params[1] - params[0]) / 10 * (p[0] - 5) + params[1])
+            and p[1] >= -((params[1] - params[0]) / 10 * (p[0] - 5) + params[1])
+            # y <= (k2 - k1) / 10 * (x - 5) + k2 and
+        ],
+        parameter_id_list=["k1", "k2"],
+        parameter_boundary_list=[(2 * K, 5 * K), (1 * K, 5 * K)],
+    )
 
-wing_params = ShapeGeneParameter(
-    label="WingShape",
-    bbox=bbox,
-    a_f=[
-        lambda p, params: p[1] >= p[0] ** 2 - params[0]
-        and p[1] <= -p[0] ** 2 + params[0],
-        # y <= x^2 - c and y <= -x^2 + c
-    ],
-    parameter_id_list=["c"],
-    parameter_boundary_list=[(2, 5)],
-)
 
-hole_params = ShapeGeneParameter(
-    label="HoldShape",
-    bbox=bbox,
-    a_f=[lambda p, params: (p[0] ** 2 + p[1] ** 2) >= params[0]],
-    parameter_id_list=["hole_r"],
-    parameter_boundary_list=[(2, 4)],
-)
+def circle_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="CircleShape",
+        bbox=bbox,
+        a_f=[
+            lambda p, params: (p[0] ** 2 + p[1] ** 2) <= params[0] ** 2,
+            # x^2 + y^2 <= r^2
+        ],
+        parameter_id_list=["r"],
+        parameter_boundary_list=[(1.5 * K, 3 * K)],
+        # 2 <= r <= 5
+    )
+
+
+def triangle_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="TriangleShape",
+        bbox=bbox,
+        a_f=[
+            lambda p, params: p[0] >= 0 and p[1] >= 0 and p[0] + p[1] <= params[0],
+            # x >= 0 and y >= 0 and x + y <= l
+        ],
+        parameter_id_list=["l"],
+        parameter_boundary_list=[(3 * K, 10 * K)],
+        # 2 <= l <= 10
+    )
+
+
+def wing_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="WingShape",
+        bbox=bbox,
+        a_f=[
+            lambda p, params: p[1] >= p[0] ** 2 - params[0]
+            and p[1] <= -p[0] ** 2 + params[0],
+            # y <= x^2 - c and y <= -x^2 + c
+        ],
+        parameter_id_list=["c"],
+        parameter_boundary_list=[(2 * K, 5 * K)],
+    )
+
+
+def hole_params() -> ShapeGeneParameter:
+    return ShapeGeneParameter(
+        label="HoldShape",
+        bbox=bbox,
+        a_f=[lambda p, params: (p[0] ** 2 + p[1] ** 2) >= params[0]],
+        parameter_id_list=["hole_r"],
+        parameter_boundary_list=[(2 * K, 4 * K)],
+    )
