ce0351bc05e789f3b146fa023bf240f94d0afd2c
Danpa_cho <danpa725@cau.ac.kr>
2024-05-22 18:48:48 +0900

    feat: add CrossoverBehavior abstract class

---
+++ src/ga/p4_crossover/crossover_behavior.py
@@ -0,0 +1,16 @@
+from abc import abstractmethod
+
+import numpy as np
+
+
+class CrossoverBehavior:
+    """ """
+
+    def __init__(self, method_name: str):
+        self.method_name = method_name
+
+    @abstractmethod
+    def crossover(
+        self, first: np.ndarray[np.float64], second: np.ndarray[np.float64]
+    ) -> np.ndarray[np.float64]:
+        raise NotImplementedError
