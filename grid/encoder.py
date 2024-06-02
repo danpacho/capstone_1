eefc77768dea992390bb6446214936bbdd915393
Eaton-Ra1000 <102717848+Eaton-Ra1000@users.noreply.github.com>
2024-06-02 10:09:06 +0900

    grid_sys_Added

---
+++ grid/encoder.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import numpy as np
+import pandas as pd
+import copy
+import random
+
+def bool2code(lst, trashIO = True):
+    bool_list = copy.deepcopy(lst)
+    
+    num_list = []
+    if len(bool_list)!= 340:
+        sys.exit('배열 길이 340 맞출 것')
+
+    for i in range(0,340):
+        if int(bool_list[i]) not in (0, 1):
+            print(i)
+            sys.exit('번째 bool값 오류')
+ 
+    for k in range(0,4):
+        for i in range(0,5):
+            row_num = 0
+            for j in range (0, k):
+                if trashIO == True:
+                    row_num = row_num + (2**(19-j) * random.randint(0,1))
+            for j in range(k, 20-k):
+                n =int(j+20*i+20*5*k-5*2*(np.sum(np.arange(0, k)))-(2*k*i)-k)
+                if bool_list[n] == 1:
+                    row_num = row_num + 2**(19-j)
+            for j in range (20-k, 20):
+                if trashIO == True:
+                    row_num = row_num + (2**(19-j) * random.randint(0,1))
+            if not (0 <= row_num <= 1048576):
+                print(i, ",", row_num)
+                sys.exit('줄 인코딩 오류')
+            num_list.append(row_num)
+        
+    return num_list
+
+def dpboolgen(vent_opened):
+    vent = np.zeros(340)
+    plot = 0
+    if vent_opened not in range (0, 341):
+        sys.exit('0~340사이 열린 벤트수 작성')
+
+    #DP list 추가
+    while plot < vent_opened:
+        i = random.randint(0, 339)
+        if vent[i] == int(0):
+            vent[i] = int(1)
+            plot = plot + 1
+
+    return vent
+
+def dpnumgen(open_num, trashIO = True):
+    vents = dpboolgen(open_num)
+    
+    return bool2code(vents, trashIO)
+    
---
+++ grid/operator.py
@@ -0,0 +1,129 @@
+import copy
+import sys
+import grid.decoder as gd
+import grid.encoder as ge
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def csv_reader(csv_name = 'dpset.csv'):
+    imported_csv = pd.read_csv(csv_name, header=None)
+    dp_list = imported_csv.values.tolist()
+    
+    return dp_list
+
+def csv_saver(my_list, file_name):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(my_list)
+
+def dp_existed_checker(new_list, existing_lists):
+    return any(new_list == existing_list for existing_list in existing_lists)
+
+def dp_add(vent_opened, dp_list, trashIO=True, monitorIO=False):
+    if vent_opened not in range (0, 341):
+        sys.exit('0~340사이 열린 벤트수 작성')
+
+    io = False
+    while io == False:
+        generated = ge.dpnumgen(vent_opened, trashIO)
+        if dp_existed_checker(generated, dp_list):
+            io = False
+            if monitorIO == True:
+                print("새로 추가된 리스트는 기존 이중 리스트에 있습니다.")
+        else:
+            io = True
+            dp_list.append(generated)
+            if monitorIO == True:
+                print("새로 추가된 리스트는 기존 이중 리스트에 없습니다.")
+
+    if monitorIO == True:
+        gd.plotter(generated)
+        print(openCounter(generated))
+
+    return dp_list
+
+def openCounter(lst):
+    if len(lst)!=20:
+        sys.exit('배열 길이 20 맞출 것')
+    counter = 0
+    num_list = copy.deepcopy(lst)
+    for i in range(0,5):
+        row_data = num_list[(19-i)]
+        for j in range(0,3):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        for j in range(3,17):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+                counter = counter + 1
+        for j in range(17,20):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        if row_data != 0:
+            print("\n", row_data)
+            sys.exit("error")
+
+    for i in range(5,10):
+        row_data = num_list[(19-i)]
+        for j in range(0,2):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        for j in range(2,18):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+                counter = counter + 1
+        for j in range(18,20):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        if row_data != 0:
+            print("\n", row_data)
+            sys.exit("error")
+
+    for i in range(10,15):
+        row_data = num_list[(19-i)]
+        for j in range(0,1):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        for j in range(1,19):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+                counter = counter + 1
+        for j in range(19,20):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+        if row_data != 0:
+            print("\n", row_data)
+            sys.exit("error")
+    for i in range(15,20):
+        row_data = num_list[(19-i)]
+        for j in range(0,20):
+            if ((row_data-2**(19-j))>=0):
+                row_data = (row_data-2**(19-j))
+                counter = counter + 1
+        if row_data != 0:
+            print("\n", row_data)
+            sys.exit("error")
+
+    return counter
+
+def dataset_analysis(dp_list):
+    dp_type_counter = np.zeros(341)
+
+    for k in range(0, len(dp_list)):
+        opened = openCounter(dp_list[(k)])
+    
+        dp_type_counter[opened] = dp_type_counter[opened] + 1
+
+    total_data_counted = 0
+
+    for i in range(0, len(dp_type_counter)):
+        total_data_counted = int(total_data_counted + dp_type_counter[i])
+        print(i, ":", dp_type_counter[i])
+
+    plt.plot(dp_type_counter)
+    plt.show()
+
+    return dp_type_counter
+
