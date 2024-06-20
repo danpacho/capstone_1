a9e51bb8d9dc6e3f2dd1d6269699dad10a468334
Danpa_cho <danpa725@cau.ac.kr>
2024-06-21 02:33:43 +0900

    feat: update model metrics, add spearman correlation

--- src/prediction/model_metrics.py
+++ src/prediction/model_metrics.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from typing import Generic, TypeVar
+from scipy.stats import spearmanr
 
 ModelType = TypeVar("ModelType")
 
@@ -13,11 +14,50 @@ class ModelMetrics(Generic[ModelType]):
         #     raise NotImplementedError
         self.test_kit = test_kit
 
-    def drag_metrics(self, drag_pred) -> float:
-        return r2_score(self.test_kit[:, 0], drag_pred)
+    def drag_metrics(self, drag_pred) -> tuple[float, float, float]:
+        """
+        Calculates the Spearman correlation, p-value and R^2 score of the drag prediction.
 
-    def ave_temp_metrics(self, ave_temp_pred) -> float:
-        return mean_squared_error(self.test_kit[:, 1], ave_temp_pred)
+        Returns:
+        rho: float
+            The Spearman correlation
+        p_val: float
+            The p-value of the Spearman correlation
+        r2: float
+            The R^2 score
+        """
+        rho, p_val = spearmanr(self.test_kit[:, 0], drag_pred)
+        r2 = r2_score(self.test_kit[:, 0], drag_pred)
+        return rho, p_val, r2
+
+    def ave_temp_metrics(self, ave_temp_pred) -> tuple[float, float, float]:
+        """
+        Calculates the Spearman correlation, p-value and Mean Squared Error of the average temperature prediction.
+
+        Returns:
+        rho: float
+            The Spearman correlation
+        p_val: float
+            The p-value of the Spearman correlation
+        mse: float
+            The Mean Squared Error
+        """
+        rho, p_val = spearmanr(self.test_kit[:, 1], ave_temp_pred)
+        mse = mean_squared_error(self.test_kit[:, 1], ave_temp_pred)
+        return rho, p_val, mse
 
     def max_temp_metrics(self, max_temp_pred) -> float:
-        return mean_absolute_error(self.test_kit[:, 2], max_temp_pred)
+        """
+        Calculates the Spearman correlation, p-value and Mean Absolute Error of the max temperature prediction.
+
+        Returns:
+        rho: float
+            The Spearman correlation
+        p_val: float
+            The p-value of the Spearman correlation
+        mae: float
+            The Mean Absolute Error
+        """
+        rho, p_val = spearmanr(self.test_kit[:, 2], max_temp_pred)
+        mae = mean_absolute_error(self.test_kit[:, 2], max_temp_pred)
+        return rho, p_val, mae
